b83c192241bdb251c6421f914ade104eb2fe29f2
chayanatik1994 <chayanatik@gmail.com>
2026-01-08 10:57:44 +0800

    Added the solution with max value'

--- index.js
+++ index.js
@@ -35,3 +35,18 @@ function isPalindrome(str) {
 
 console.log(isPalindrome("madam")); 
 console.log(isPalindrome("hello")); 
+
+//Problem-04 - find the  max Number
+function findMax(numbers) {
+  let max = numbers[0];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }
+  }
+  return max;
+}
+
+console.log(findMax([5, 1, 9, 3])); 
+
