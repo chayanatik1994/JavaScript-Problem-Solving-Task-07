e6019441783118124b4bcac787842692b1e076e6
chayanatik1994 <chayanatik@gmail.com>
2026-01-08 11:30:01 +0800

    added the solution of capitalizewords'

--- index.js
+++ index.js
@@ -93,3 +93,17 @@ function findEvenNumbers(arr) {
 
 console.log(findEvenNumbers([1, 2, 3, 4, 5, 6])); 
 
+//Problem 08
+function capitalizeWords(str) {
+  let words = str.split(" ");
+  let result = [];
+
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    let capitalized = word[0].toUpperCase() + word.slice(1);
+    result.push(capitalized);
+  }
+  return result.join(" ");
+}
+
+console.log(capitalizeWords("hello world")); 
