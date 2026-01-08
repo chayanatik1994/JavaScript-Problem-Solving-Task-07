76401f8c471c606e4790f1cb7a75ecb3c66f8932
chayanatik1994 <chayanatik@gmail.com>
2026-01-08 11:02:27 +0800

    Added the solution of dupluicate array'

--- index.js
+++ index.js
@@ -50,3 +50,17 @@ function findMax(numbers) {
 
 console.log(findMax([5, 1, 9, 3])); 
 
+//Problem-05 - Remove Duplicates from Array
+
+function removeDuplicates(arr) {
+  let unique = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!unique.includes(arr[i])) {
+      unique.push(arr[i]);
+    }
+  }
+  return unique;
+}
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4])); 
