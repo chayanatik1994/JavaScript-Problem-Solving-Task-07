03476d8181c2d0e3e3ca6754d9a058cedd549648
chayanatik1994 <chayanatik@gmail.com>
2026-01-08 11:22:51 +0800

    added problem 07 with solution

--- index.js
+++ index.js
@@ -79,4 +79,17 @@ function sumArray(arr) {
 console.log(sumArray([1, 2, 3, 4])); 
 
 
+//Problem - 07 Find Even Numbers
+function findEvenNumbers(arr) {
+  let evens = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      evens.push(arr[i]);
+    }
+  }
+  return evens;
+}
+
+console.log(findEvenNumbers([1, 2, 3, 4, 5, 6])); 
 
